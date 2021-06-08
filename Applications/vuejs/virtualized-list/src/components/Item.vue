90228b62309ed7e9edc701c2e3cc3527633f80ab
Jae Park <jaep@criticalmass.com>
2021-06-07 23:16:40 -0400

    initial file setup for virtualized-list

---
+++ Applications/vuejs/virtualized-list/src/components/Item.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="image-item">
+        <p>Item number - {{item.id}}</p>
+        <!-- <img :src="require('imageItem.url')"> -->
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Item',
+        props: ['item']
+    }
+</script>
